94968baace8610d76ffa636177c6c125d010cb02
Aleksandr <spec.besp.89@gmail.com>
2025-01-15 22:08:00 +0800

    Исправлены проблемы с startup.sh и миграциями

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,6 @@ COPY requirements.txt /app/requirements.txt
 RUN pip install --no-cache-dir -r /app/requirements.txt
 
 COPY . /app
-
 RUN chmod +x /app/startup.sh /app/wait-for-it.sh
 
 ENV PYTHONUNBUFFERED=1
--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Waiting for PostgreSQL to start..."
 /app/wait-for-it.sh db 5432 --timeout=30 --strict -- echo "PostgreSQL is up - executing command"
 
 echo "Applying database migrations..."
 python manage.py migrate
-
+set -x
 echo "Starting server..."
 exec python manage.py runserver 0.0.0.0:8000
